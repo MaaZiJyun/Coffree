1e2d0bc614dd44ee24c2bfd9cc815cf95a495787
MA ZHIYUAN <zy.maa.social@outlook.com>
2022-06-27 01:41:26 +0800

    initial

---
+++ EACaffree-war/src/java/com/caffree/ejb/AddServlet.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.caffree.ejb;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+/**
+ *
+ * @author 37409
+ */
+@WebServlet(name = "AddServlet", urlPatterns = {"/AddServlet"})
+public class AddServlet extends HttpServlet {
+    
+}
